47e57509543278cc13bcec329cc112a7a2906a44
Jan Griesfeller <jgriesfeller@users.noreply.github.com>
2026-09-25 11:38:43 +0000

    1st working version (od550aer)

--- OsloCTM3v1-2-r1/OsloCTM3v1-2-r1_AP4-CTRL-PD-NudTran/rename.sh
+++ OsloCTM3v1-2-r1/OsloCTM3v1-2-r1_AP4-CTRL-PD-NudTran/rename.sh
@@ -4,6 +4,16 @@ if [[ $# -eq 0 ]]
    exit 1
 fi
 
+# tools, because mamba run -n cdo cd is massively slower
+# e.g. cdo=$(mamba run -n cdo which cdo) would also work, but is also quite slow
+cdo='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/cdo/bin/cdo'
+ncks='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/nco/bin/ncks'
+ncdump='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/nco/bin/ncdump'
+ncrename='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/nco/bin/ncrename'
+ncap2='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/nco/bin/ncap2'
+ncatted='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/nco/bin/ncatted'
+ncwa='/modules/rhel8/user-apps/aerocom/miniforge-23.11/envs/nco/bin/ncwa'
+
 
 #we assume that the downloaded data is organiosed as 
 # <basedir>/<modeldir>/download/<experiment_id>/filename.nc
@@ -19,80 +29,79 @@ updateflag=0
 date=$(date '+%Y%m%d_%H%M%S')
 RND=${RANDOM}
 tmpfile="./dummy_${RND}.nc"
-cmd_nco="ncks -O -4 --chunk_policy nco"
-cmd_cdo="cdo -O -f nc4 -k auto copy"
-threedcmd="cdo -O -f nc4 -k auto copy"
+cmd_nco="${ncks} -O -4 --chunk_policy nco"
+cmd_cdo="${cdo} -O -f nc4 -k auto copy"
+threedcmd="${cdo} -O -f nc4 -k auto copy"
 
-# That's the config: var_name:aerocom_var_name:lowest level
-3d_arr=(\
-"co:concco:1" \
-"hno3:vmrhno3:1" \
-"no2:concno2:1" \
-"no:concno:1" \
-"o3:vmro3:1" \
-"so2:concso2:1" \
-)
+##### Model specific part start
+lowest_level=1
+# All vars we can handle should be in here
+declare -A aerocom_vars
+aerocom_vars[o3]="vmro3"
+aerocom_vars[mmrso4]="mmrso4"
+aerocom_vars[ta]="ts"
+aerocom_vars[pfull]="ps"
+aerocom_vars[no2]="vmrno2"
+aerocom_vars[so2]="vmrso2"
+aerocom_vars[no]="vmrno"
+aerocom_vars[co]="vmrco"
+aerocom_vars[nh3]="vmrnh3"
 
-# var lists
-#make a varlist from user friendly definition above
-for ((i=0; i < ${#3d_arr[*]}; i += 1))
-   do CciVars[${i}]=`echo ${3d_arr[${i}]} | cut -d= -f2`
-   AerocomVars[${i}]=`echo ${3d_arr[${i}]} | cut -d= -f1`
-done
 
-echo ${CciVars[*]}
-echo ${#CciVars[*]}
+##### Model specific part end
 
 
 set -x
 for file in "$@"
    do echo ${file}
+   if [[ ! -e ${file} ]]
+      then echo "${file} not found! skipping..."
+      continue
+   fi
    cmd=${cmd_nco}
+   threedflag=0
    # special treatment for 3d files...
    if [[ ${file} =~ .*Level_.* ]]
       then
       cmd=${threedcmd}
+      threedflag=1
    fi
 
    _var=$(basename ${file} | rev | cut -d_ -f4 | rev)
-   
+   _file_rest=$(echo ${file} | rev | cut -d_ -f1-3 | rev)
+   _basedir=$(dirname ${file})
+   outdir=$(echo ${_basedir} | sed -e "s/download/renamed/g")
+   if [[ ! -d ${outdir} ]]
+      then 
+      mkdir -p ${outdir}
+   fi
+   break_flag=0
+   if [[ -v aerocom_vars[${_var}] ]]
+   then
+      _aerocom_var=${aerocom_vars[${_var}]}
+   else
+      _aerocom_var=${_var}
+   fi
+   outfile="${outdir}/${Model}_${_aerocom_var}_${_file_rest}"
+   if [[ -e ${outfile} ]]
+      then
 
-expname=`dirname ${file} | rev | cut '-d/' -f1 | rev`
-#adjust the experiment name to standard
-if [[ ${expname} == 'EXP-SS-DUST' ]]
-then
-expname='EXP-DUST-SS'
-fi
+      if [[ ${updateflag} -eq 0 ]]
+         then
+         echo "warning: target file ${outfile} exists! skipping..."
+         continue
+      else
+         echo "updating target file ${outfile}"
+      fi
+   fi
 
-#outfile=`basename ${file}`
-#outfile="./${outfile}3"
-outfile=`basename ${file} | sed -e "s/MATCH/MATCH\.cams61\.${expname}/g"`
-outmodelname=`echo ${outfile} | cut -d_ -f2`
-outdir="${basedir}/${outmodelname}/renamed/"
-if [[ ! -d ${outdir} ]]
-then 
-mkdir -p ${outdir}
-fi
-outfile="${basedir}/${outmodelname}/renamed/${outfile}"
-if [[ ! -e ${outfile} ]]
-then echo "output file: ${outfile}"
-${cmd} ${file} ${outfile}
-else
-if [[ ${updateflag} -eq 0 ]]
-then
-echo "warning: target file ${outfile} exists! skipping..."
-else
-echo "updating target file ${outfile}"
-backupoutdir="${basedir}/${outmodelname}/backup/"
-if [[ ! -d ${backupoutdir} ]]
-		  then
-		  mkdir -p ${backupoutdir}
-fi
-backupfile=`basename ${outfile}`
-mv ${outfile} "${backupoutdir}/${backupfile}.${date}"
-${cmd} ${file} ${outfile}
-fi
-fi
+   if [[ ${threedflag} -eq 1 ]]
+   then #3d file: extract lowest layer
+      ${cmd} ${file} ${outfile}
+   else
+      ${cmd} ${file} ${outfile}
+   fi
+
 
 done
 
